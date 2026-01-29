262ea56b19d9d71ce457d6f97600dadd1640a11a
Lucas Maia <lucasm@uia.no>
2026-01-29 09:50:42 +0100

    first commit

---
+++ week5.mjs
@@ -0,0 +1,3 @@
+//The Simple Calculator:
+// Write a test suite for a multiply(a, b) function. Test that the function correctly multiplies two positive integers.What happens if one of the numbers is 0? Or a negative number?
+
